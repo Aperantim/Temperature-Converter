706d69a1bb9d3969a7ede95d99efec5f25dbf888
Vitaliy Nesterov <liyuop@mail.ru>
2020-01-10 17:56:38 +0300

    Initial Commit

---
+++ Temperature Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Temperature Converter
+//
+//  Created by Виталий on 10.01.2020.
+//  Copyright © 2020 Aperantim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
